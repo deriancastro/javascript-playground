c8e79040c5ca5a162b48bed883289dac40659657
Derian Castro <deriancastro@hotmail.com>
2021-04-27 12:32:34 +0200

    Finish first exercise

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-import AppHeader from './components/AppHeader'
-
-document.body.append(AppHeader('JavaScript App Template'))
+console.clear()
+const box = document.querySelector('.box')
+const buttonAdd = document.querySelector('.button-add')
+const buttonRemove = document.querySelector('.button-remove')
+const buttonToggle = document.querySelector('.button-toggle')
+buttonAdd.addEventListener('click', () => {
+  box.classList.add('blue')
+})
+buttonRemove.addEventListener('click', () => {
+  box.classList.remove('blue')
+})
+buttonToggle.addEventListener('click', () => {
+  box.classList.toggle('blue')
+})
