fbe3713da5906d14c161d6931bd6a16b95a3797e
Derian Castro <deriancastro@hotmail.com>
2021-04-30 12:51:29 +0200

    Boxes for each

--- src/index.js
+++ src/index.js
@@ -1,14 +1,9 @@
-console.clear()
-const box = document.querySelector('.box')
-const buttonAdd = document.querySelector('.button-add')
-const buttonRemove = document.querySelector('.button-remove')
-const buttonToggle = document.querySelector('.button-toggle')
-buttonAdd.addEventListener('click', () => {
-  box.classList.add('blue')
-})
-buttonRemove.addEventListener('click', () => {
-  box.classList.remove('blue')
-})
-buttonToggle.addEventListener('click', () => {
-  box.classList.toggle('blue')
+import getAllElements from './utils/getAllElements'
+
+const boxes = getAllElements('.box')
+
+boxes.forEach(box => {
+  box.addEventListener('click', () => {
+    box.classList.toggle('blue')
+  })
 })
